b99c78b8c2c52df1bbebb918eef47228cec63d79
alex <alexanderrosen45@gmail.com>
2022-10-06 21:42:36 -0400

    more bug fixes

--- assignment1.py
+++ assignment1.py
@@ -10,10 +10,10 @@ def significand_value(n: int, permutation: list[int]) -> float:
         significand_value += float(permutation[i])*2**(-i-1)
     return significand_value
 
-def check_exponent_value(max_value: int, target: int, permutations: list[int]):
+def check_exponent_value(max: int, min: int, t: int, permutations: list[int]):
     import math
     for permutation in permutations:
-        if math.log(target/permutation, 2) in range(max_value):
+        if math.log(t/permutation, 2) in range(min, max):
             return (math.log(target/permutation, 2), permutation)
     return False
 
@@ -28,11 +28,14 @@ if __name__ == '__main__':
         for permutation in p
     ]
 
-    # check if 17 divided by each permutation yields any integer power of 2
-    target = 17
+    target = 15
     exponent_max_value = 8 # 15 (1111 from binary to decimal) minus 2^(4-1)-1=7
+    exponent_min_value = -7 # 0 (0000 from binary to decimal) minus 2^(4-1)-1=7
+
+    # check if 17 divided by each permutation yields any integer power of 2
     exponent_value = check_exponent_value(
         exponent_max_value,
+        exponent_min_value,
         target,
         significand_permutations
     )
@@ -40,7 +43,7 @@ if __name__ == '__main__':
     if exponent_value == False:
         print("no solutions")
     else:
-        if exponent_value[1] in range(exponent_max_value):
+        if int(exponent_value[1]) in range(exponent_min_value, exponent_max_value+1):
             print("Yes, using the exponent value 2^"+str(exponent_value[0])
             +" and the significand value "+str(exponent_value[1]))
         else:
